5d19c0bb2c0f87f36f81f64ec9cd25d1ada6af96
unknown <wscorralestr@gmail.com>
2019-11-08 16:54:56 -0500

    agregar app.py

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def update_monthly(exchange, leverage, start_date, end_date):
 ##########
 ##################################################
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
